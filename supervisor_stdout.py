9bdf630d0d1b4a16cf8b60b96adafb1ed98e7380
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2020-06-26 14:03:03 -0400

    Correctly process event messages to/from supervisord as bytes instead of str

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -1,13 +1,16 @@
 from __future__ import print_function
 import sys
 
+
 def write_stdout(s):
     sys.stdout.write(s)
     sys.stdout.flush()
 
+
 def write_stderr(s):
     sys.stderr.write(s)
     sys.stderr.flush()
+
 
 def main():
     while 1:
@@ -15,14 +18,16 @@ def main():
         line = sys.stdin.readline()  # read header line from stdin
         headers = dict([ x.split(':') for x in line.split() ])
         data = sys.stdin.read(int(headers['len'])) # read the event payload
-        write_stdout('RESULT %s\n%s'%(len(data), data)) # transition from READY to ACKNOWLEDGED
+        write_stdout('RESULT %s\n%s'%(len(data.encode('utf-8')), data)) # transition from READY to ACKNOWLEDGED
+
 
 def event_handler(event, response):
-    line, data = response.split('\n', 1)
-    headers = dict([ x.split(':') for x in line.split() ])
-    lines = data.split('\n')
-    prefix = '%s %s | '%(headers['processname'], headers['channel'])
-    print('\n'.join([ prefix + l for l in lines ]))
+    line, data = response.split(b'\n', 1)
+    headers = dict([ x.split(b':') for x in line.split() ])
+    lines = data.split(b'\n')
+    prefix = b'%s %s | '%(headers[b'processname'], headers[b'channel'])
+    print(b'\n'.join([ prefix + l for l in lines if l]).decode('utf-8'))
+
 
 if __name__ == '__main__':
     main()
